37f16f0de424eac41f44fc5149528469c725a949
Alessandro <33810424+ACINIELLO@users.noreply.github.com>
2020-12-05 15:50:03 -0500

    Add files via upload

--- CompVision./Aer817project_AvionicsBayCamAnalysis.c
+++ CompVision./Aer817project_AvionicsBayCamAnalysis.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h> //to use true and false
+#include <math.h>
+
 /*
         REFERENCES:
 rgb to grayscale: 
@@ -13,8 +15,6 @@ rgb to grayscale:
 finding jpeg image width n height: 
 i.https://stackoverflow.com/questions/18264357/how-to-get-the-width-height-of-jpeg-file-without-using-library
 ii.https://web.archive.org/web/20131016210645/http://www.64lines.com/jpeg-width-height
-
-4. using library to process image in C: https://processing.org/tutorials/pixels/
 
 
 */
@@ -29,107 +29,86 @@ ii.https://web.archive.org/web/20131016210645/http://www.64lines.com/jpeg-width-
 */
 
 
+//unsigned char GRAY[];
+float pixel_2;
 
-
-
-
-
-
-
-// -------------from ref. 1:
-// from ref. #3 ii.:
-
-//Gets the JPEG size from the array of data passed to the function, file reference: http://www.obrador.com/essentialjpeg/headerinfo.htm
-/*
-static char get_jpeg_size(unsigned char *data, unsigned int data_size, unsigned short *widthJPEG, unsigned short *heightJPEG)
-{
-
-    //Check for valid JPEG image
-    int i = 0; // Keeps track of the position within the file
-    if (data[i] == 0xFF && data[i + 1] == 0xD8 && data[i + 2] == 0xFF && data[i + 3] == 0xE0)
-    {
-        i += 4;
-        // Check for valid JPEG header (null terminated JFIF)
-        if (data[i + 2] == 'J' && data[i + 3] == 'F' && data[i + 4] == 'I' && data[i + 5] == 'F' && data[i + 6] == 0x00)
-        {
-            //Retrieve the block length of the first block since the first block will not contain the size of file
-            unsigned short block_length = data[i] * 256 + data[i + 1];
-            while (i < data_size)
-            {
-                i += block_length; //Increase the file index to get to the next block
-                if (i >= data_size)
-                    return false; //Check to protect against segmentation faults
-                if (data[i] != 0xFF)
-                    return false; //Check that we are truly at the start of another block
-                if (data[i + 1] == 0xC0)
-                { //0xFFC0 is the "Start of frame" marker which contains the file size
-                    //The structure of the 0xFFC0 block is quite simple [0xFFC0][ushort length][uchar precision][ushort x][ushort y]
-                    *heightJPEG = data[i + 5] * 256 + data[i + 6];
-                    *widthJPEG = data[i + 7] * 256 + data[i + 8];
-                    return true;
-                }
-                else
-                {
-                    i += 2;                                     //Skip the block marker
-                    block_length = data[i] * 256 + data[i + 1]; //Go to the next block
-                }
-            }
-            return false; //If this point is reached then no size was found
-        }
-        else
-        {
-            return false;
-        } //Not a valid JFIF string
-    }
-    else
-    {
-        return false;
-    } //Not a valid SOI header
-   
-} 
-*/
 // converting image to grayscale:
-    int main()
+int main()
+{
+    // importing image:
+    // 16 bit bitmap contains more shades/colour intensity ie- more steps to white,  then 8 or 4bit==> https://www.photoshopessentials.com/essentials/16-bit/#:~:text=The%20answer%20goes%20back%20to,to%2065%2C536%20shades%20of%20gray.
+
+    FILE *fIn = fopen("HumanIRimage_2_24bit_QCIF.bmp", "rb");
+    //FILE *fIn2 = fopen("HumanThermalimage_3.bmp", "rb");
+
+    FILE *fOut = fopen("HumanIRimage_2_gray_QCIF.bmp", "wb");
+    if (!fIn || !fOut) //||!fIn2)
     {
-        // importing image:
-
-        FILE *fIn = fopen("HumanThermalimage_3.bmp", "rb");
-        FILE *fOut = fopen("HumanThermalimage_3_gray.bmp", "wb");
-        if (!fIn || !fOut)
-        {
-            printf("File error.\n");
-            return 0;
-        }
-
-        unsigned char header[54];
-        fread(header, sizeof(unsigned char), 54, fIn);
-        fwrite(header, sizeof(unsigned char), 54, fOut);
-
-        int width = *(int *)&header[18];
-        int height = abs(*(int *)&header[22]);
-        int stride = (width * 3 + 3) & ~3;
-        int padding = stride - width * 3;
-
-        printf("width: %d (%d)\n", width, width * 3);
-        printf("height: %d\n", height);
-        printf("stride: %d\n", stride);
-        printf("padding: %d\n", padding);
-
-        unsigned char pixel[3];
-        for (int y = 0; y < height; ++y)
-        {
-            for (int x = 0; x < width; ++x)
-            {
-                fread(pixel, 3, 1, fIn);
-                unsigned char gray = pixel[0] * 0.299 + pixel[1] * 0.587 + pixel[2] * 0.114; // rgb eqn.(pixel[0]=r,pixel[1]=g,pixel[3]=b) <== eqn from ref. #2
-                memset(pixel, gray, sizeof(pixel));
-                fwrite(&pixel, 3, 1, fOut);
-            }
-            fread(pixel, padding, 1, fIn);
-            fwrite(pixel, padding, 1, fOut);
-        }
-        fclose(fOut);
-        fclose(fIn);
+        printf("File error.\n");
         return 0;
     }
 
+    unsigned char header[54];
+    fread(header, sizeof(unsigned char), 54, fIn);
+    fwrite(header, sizeof(unsigned char), 54, fOut);
+
+    int width = *(int *)&header[18];
+    int height = abs(*(int *)&header[22]);
+    int stride = (width * 3 + 3) & ~3;
+    int padding = stride - width * 3;
+
+    printf("width: %d (%d)\n", width, width * 3);
+    printf("height: %d\n", height);
+    printf("stride: %d\n", stride);
+    printf("padding: %d\n", padding);
+    printf(" header %hhX \n", header);
+    unsigned char pixel[3];
+    
+
+    unsigned char gray, yellow;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            fread(pixel, 3, 1, fIn);  //read 1st 3 bits of fin and assign to pixel
+            if (pixel[0] <= 165)
+            {
+                pixel[0] = 0;
+            }
+            if (pixel[1] <= 165)
+            {
+                pixel[1] = 0;
+            }
+            if (pixel[2] <= 165)
+            {
+                pixel[2] = 0;
+            }
+            unsigned char gray = pixel[0] * 31 + pixel[1] * 0.59 + pixel[2] * 0.11; // rgb eqn.(pixel[0]=r,pixel[1]=g,pixel[3]=b) <== eqn from ref. #2
+            memset(pixel, gray, sizeof(pixel)); // assign #sizeof(pixel) gray value to  in pixel array
+           
+            fwrite(&pixel, 3, 1, fOut);
+
+            
+            /*
+            if ((pixel[0] <= 150) && (pixel[1]<=150) && (pixel[2]>=0))
+            {
+             pixel[0] =0;
+             pixel[1] =0;
+             pixel[2] =0;   
+            }
+
+            */
+
+            
+        }
+       fread(pixel, padding, 1, fIn);//read pixels in height
+       fwrite(pixel, padding, 1, fOut);
+    }
+    fclose(fOut);
+
+    fclose(fIn);
+
+    return 0;
+}
+   
--- CompVision./Aer817project_GroundstationCamAnalysis.py
+++ CompVision./Aer817project_GroundstationCamAnalysis.py
@@ -29,147 +29,55 @@ import numpy as np
 import cv2
 import time
 
- 
-# assuming the image comes back as a grayscale:
-#note: use image's 1 and 2 for testing to detect yellow hsv colour ranges and use image 3 for contour testing
-gray_img = cv2.imread("D:\Alessandro\FILESFORSCHOOL\AER817\HumanThermalimage_3_gray.bmp")
-colour_img = cv2.imread("D:\Alessandro\FILESFORSCHOOL\AER817\HumanThermalimage_3.bmp")
-
-#adding some colour:==>from==> https://stackoverflow.com/questions/58135918/giving-grayscale-image-color
-'''
-b, g, r = cv2.split(gray_img)
-
-np.multiply(b, 1.5, out=b, casting="unsafe")
-np.multiply(g, .75, out=g, casting="unsafe")
-np.multiply(r, 1.25, out=r, casting="unsafe")
-
-colour_img = cv2.merge([b, g, r])
-
-cv2.imshow('gray img', gray_img)
-cv2.imshow('colour img', colour_img)
-'''
-# openCV stuff from==> https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html#
-
-#convert to hsv for image processing:
-#cv2.imshow('gray img', gray_img)
+Method = 'M' # M indicates main code(how it should look like in the end), 'E' indicates experimental (how data the handling actually is)
 
 
-
-hsv_grayimg = cv2.cvtColor(gray_img, cv2.COLOR_BGR2HSV)
-#cv2.imshow("hsv gray image", hsv_grayimg)
-
-#hsv_colourimg = cv2.cvtColor(colour_img, cv2.COLOR_BGR2HSV)
-#cv2.imshow("hsv colour image", hsv_colourimg)
-
-#thresholding to only display yellow colour(similar to human skin temp emission):
-
-# Starting with 100's to prevent error while masking
-h_lower=s_lower=v_lower=h_upper=s_upper=v_upper=0 
-# Creating track bar
-
-#for lower bounds:
-
-
-def nothing(x):
-    pass
-
-
-
-hsvTest = 'N'
-
-if (hsvTest == 'Y'):
-
-    # Creating a window for later use (in order to see track bar)
-
-    #for upper bounds:
-    cv2.namedWindow('result')
-    cv2.createTrackbar('h_lower', 'result', 0, 179,nothing) # ranges H:0-179,S=0-255,v=0-255
-    cv2.createTrackbar('s_lower', 'result', 0, 255,nothing)
-    cv2.createTrackbar('v_lower', 'result', 0, 255,nothing)
-
-
-    cv2.createTrackbar('h_upper', 'result', 0, 179,nothing)  # ranges H:0-180,S=0-255,v=0-255
-    cv2.createTrackbar('s_upper', 'result', 0, 255,nothing)
-    cv2.createTrackbar('v_upper', 'result', 0, 255,nothing)
-
-while(1):
-            
-    if (hsvTest == 'Y'):
-        # get info from track bar and apply to result for testing:
-        
-        #for lower bounds:
-        h_lower = cv2.getTrackbarPos('h_lower', 'result')
-        s_lower = cv2.getTrackbarPos('s_lower', 'result')
-        v_lower = cv2.getTrackbarPos('v_lower', 'result')
-
-        #for upper bounds:
-        h_upper = cv2.getTrackbarPos('h_upper', 'result')
-        s_upper = cv2.getTrackbarPos('s_upper', 'result')
-        v_upper = cv2.getTrackbarPos('v_upper', 'result')
-
-    #applying the trackbar values:
-    ''' 
-    yellow hsv ranges are:
-    lowerBound = (28,58,78)
-    upperBound = (31,255,255)
     
-    '''
-    yellowLowerBound = [28, 58, 78]
-    yellowUpperBound = [31, 255, 255]
-    
-    lowerBound = np.array(yellowLowerBound) # array numbers from(3rd array ("v") represents brightness lvl, usually want range to be in betw. 20 and 255)==> https://stackoverflow.com/questions/48528754/what-are-recommended-color-spaces-for-detecting-orange-color-in-open-cv
 
-    upperBound = np.array(yellowUpperBound)# want to cover whole range of colour "rectangle" (so use corner to corner values)
+if Method == 'M':
+    colour_img = cv2.imread("D:\Alessandro\FILESFORSCHOOL\AER817\HumanIRimage_2_gray_QCIF.bmp")
 
-  
+    #convert to grayscale
+    gray = cv2.cvtColor(colour_img, cv2.COLOR_BGR2GRAY)
+    gray2 = gray
 
-    # Threshold the HSV image to get only yellow colors
-    mask = cv2.inRange(hsv_grayimg, lowerBound, upperBound)
+    #display original and grayscale images
+    cv2.imshow('original image', colour_img)
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(hsv_grayimg, hsv_grayimg, mask=mask) #only displays pixel if both images have the same pixel location and colour as tha mask 
 
-    #cv2.imshow('yellow mask', mask)
-    cv2.imshow('original colour image', colour_img)
+    #Go pixel- by pixel and find "hotspots"
+    rows, cols = gray.shape
+    for i in range(rows):
+        for j in range(cols):
+            k = gray[i, j]
 
-    cv2.imshow('hsv result', res)
-    cv2.imshow('original gray image', gray_img)
-
-    cv2.imshow('hsv gray image', hsv_grayimg)
-
-    #apply contour algorithm to detect human body shape:
-    '''
-    References for detecting human body contour:
-    
-    1. https://stackoverflow.com/questions/51705792/how-to-contour-human-body-in-an-image-using-ppencv
-    2.https://docs.opencv.org/3.1.0/db/d5c/tutorial_py_bg_subtraction.html
-    3.https://github.com/quanhua92/human-pose-estimation-opencv/blob/master/openpose.py
-    
-    
-    
-    '''
-    
+            if (gray[i, j] <= 150): #150 is rgb value for lowest bound gray image
+                heatCoordinates = [i, j]
+                gray2[i, j] = 0
+                
+    cv2.imshow('grayed image processed 2nd time', gray2)
 
     # Search for edges in the image with cv2.Canny().
-    edges = cv2.Canny(res, 150, 200)
+    edges = cv2.Canny(gray2, 100, 200)
 
     # Search for contours in the edged image with cv2.findContour().
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     # Filter out contours that are not in your interest by applying size criterion.
+    
     for cnt in contours:
         size = cv2.contourArea(cnt)
-        if size > 100:
-            cv2.drawContours(res, [cnt], -1, (255, 0, 0), 3)
-    
-    cv2.imshow('contour result', res)
+        cv2.drawContours(gray2, [cnt], -1, (255, 0, 0), 3)
+
+    cv2.imshow('contour result', gray2)
+
+    cv2.imshow('edges result', edges)
 
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
 
-cv2.destroyAllWindows()
+    if cv2.waitKey(1000000) & 0xFF == ord('q'):
 
+        cv2.destroyAllWindows()
 
 
 
